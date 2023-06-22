4b8e5f57864b3f30fc8c19311c58052827e3bf33
Franco Pignatelli <francopignatelli17@hotmail.com>
2023-06-22 20:34:13 -0300

    -Creada carpeta "ViewModels" dentro de la carpeta "Data". -Creada la clase CompraViewModel. -Agregada funcionalidad que muestra todas las compras realizadas en la pagina HistorialCompras. -Creada hoja de estilos de la pagina HistorialCompras ("HistorialCompras.razor.css"). -Modificado el index.razor para cambiar el Delay del loading indicato. para agregar el href a la hoja de estilos. -Modificado el componente WindowsNavMenu.razor para que al hacer clic en Inventario se dirija a la pag de HistorialCompras (provisionalmente para testing). -Modificado el index.html para agregar el href a la hoja de estilos.

---
+++ GVM/Data/ViewModels/CompraViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GVM.Data.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVM.Data.DTOs
+{
+    public class CompraViewModel
+    {
+        public int compraId { get; set; }
+        public Empleado empleado { get; set; }
+        public Distribuidor distribuidor { get; set; }
+        public DateTime fecha { get; set; }
+        public double monto { get; set; }
+        public string descripcion { get; set; }
+    }
+}
