5c9831b2cb55ce564bf8196ed32f969117774522
Franco Pignatelli <francopignatelli17@hotmail.com>
2023-06-27 15:29:01 -0300

    Agregado cambios parciales detalleCompra para resolver conflicos.

---
+++ GVM/Data/ViewModels/ProductoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVM.Data.ViewModels {
+    public class ProductoViewModel 
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public double Medida { get; set; }
+        public string Marca { get; set; }
+        public double Precio { get; set; }
+    }
+}
