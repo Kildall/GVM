acdbd295d77e2936f4c208a57411ad7c0f1dd239
Pedro B <pebustos.135@gmail.com>
2023-07-11 00:37:04 -0300

    Agregado dato de venta a pagina detalle venta

--- GVM/Data/ViewModels/VentaViewModel.cs
+++ GVM/Data/ViewModels/VentaViewModel.cs
@@ -8,10 +8,10 @@ namespace GVM.Data.ViewModels {
     public class VentaViewModel {
         public int VentaId { get; set; }
         public int ClienteId { get; set; }
-        public EstadoVentaViewModel Estado { get; set; }
+        public EstadoVentaViewModel Estado { get; set; } = new();
         public DateTime FechaInicio { get; set; }
         public DateTime FechaUltimaActualizacion { get; set; }
-        public ClienteViewModel Cliente { get; set; }
+        public ClienteViewModel Cliente { get; set; } = new ();
         public List<ProductoViewModel> Productos { get; set; } = new();
     }
 }
