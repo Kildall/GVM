4e84951901b5d95aaf62b418b90b11140178ec30
Franco Pignatelli <francopignatelli15@gmail.com>
2024-09-29 18:10:16 -0300

    add: agregada clase generica response

---
+++ GVM/Data/Response/LoginResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVM.Data.Response {
+    public class LoginResponse : Response {
+        public new LoginData data { get; set; }
+
+        public class LoginData {
+            public DateTime expires { get; set; }
+            public string token { get; set; }
+            public bool verified { get; set; }
+        }
+    }
+}
---
+++ GVM/Data/Response/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+
+namespace GVM.Data.Response {
+    public abstract class Response {
+        public ResponseStatus status { get; set; }
+        public object data { get; set; }
+
+        public class ResponseStatus {
+            public bool success { get; set; }
+            public object[] errors { get; set; }
+        }
+    }
+}
